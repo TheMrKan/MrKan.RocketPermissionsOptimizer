abb126c2add9099c4b17e9b40bb6d0af8c01c2fa
TheMrKan <mrkan2017@gmail.com>
2025-03-20 23:32:16 +0400

    Добавьте файлы проекта.

---
+++ CachedPermissionsHelper.cs
@@ -0,0 +1,37 @@
+﻿using Rocket.API;
+using Rocket.API.Serialisation;
+using Rocket.Core;
+using Rocket.Core.Assets;
+using Rocket.Core.Permissions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+#nullable enable
+namespace MrKan.RocketPermissionsOptimizer
+{
+    public class CachedPermissionsHelper
+    {
+        private readonly object m_PermissionsHelper;
+        private readonly Asset<RocketPermissions> m_PermissionsAsset;
+        internal CachedPermissionsHelper()
+        {
+            m_PermissionsHelper = GetPermissionsHelper();
+            m_PermissionsAsset = GetPermissionsAsset(m_PermissionsHelper);
+        }
+
+        private static object GetPermissionsHelper()
+        {
+            var permissionHelperField = typeof(RocketPermissionsManager).GetField("helper", BindingFlags.Instance | BindingFlags.NonPublic);
+            return permissionHelperField.GetValue(R.Permissions);
+        }
+
+        private static Asset<RocketPermissions> GetPermissionsAsset(object permissionsHelper)
+        {
+            return (Asset<RocketPermissions>)permissionsHelper.GetType().GetField("permissions", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(permissionsHelper);
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Rocket.Core;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrKan.RocketPermissionsOptimizer
+{
+    public class RPOptimizerPlugin : RocketPlugin<Config>
+    {
+        public static RPOptimizerPlugin? Instance { get; private set; }
+        public static CachedPermissionsHelper Helper { get; private set; }
+
+        private Harmony m_Harmony;
+        protected override void Load()
+        {
+            Instance = this;
+
+            m_Harmony = new Harmony("MrKan.RocketPermissionsOptimizer");
+            m_Harmony.PatchAll();
+
+            Helper = new();
+        }
+
+        protected override void Unload()
+        {
+            m_Harmony.UnpatchAll("MrKan.RocketPermissionsOptimizer");
+
+            Instance = null;
+        }
+    }
+}
